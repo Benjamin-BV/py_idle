87589fac67f3afc231af853583fadd4295077882
Benjamin-BV <benjamin.bradleyvokey@gmail.com>
2024-08-25 20:42:15 -0400

    Added to classes Added a bit more functionality to classes, still not implemented

--- subdir/classes.py
+++ subdir/classes.py
@@ -4,15 +4,32 @@ class buyable:
         self.cost = cost
         self.money = money
         self.cost_factor = cost_factor
-
-
+   
+### CLASS THAT CONTAINS PLAYER DATA ###
+class player:
+    def callMoney(money):
+        player.money = money
+        return player.money 
 
 ###PSEUDOCODE FOR UPGRADE CLASS###
-#
-#class upgrade:
-#    def set(self, cost, buyable, effect, money):
-#        self.cost = cost
-#        if(effect == "exp"):
-#            buyable.money **= money
-#        elif(effect == "mult"):
-#            buyable.money *= money 
+#not functional yet, might scrap constructors for now and be a lazy shit, WE LOVE HARDCODING
+class upgrade:
+    #Setting cost parameters for the upgrade
+    def set(self, cost, buyable, effect, magnitude, name, null):
+        self.cost = cost
+        if(effect == "exp"):
+            buyable.money **= magnitude
+        elif(effect == "mult"):
+            buyable.money *= magnitude 
+
+        #Text description for the upgrade, seperate bc it gets wordy if in just one function call
+    def description(self, text):
+        
+        #Create a label that changes based on the hover of an upgrade
+        pass
+
+        #Checking if player can afford upgrade
+    def affordable(self, cost):
+        if(player.money > self.cost):
+            return True
+    
